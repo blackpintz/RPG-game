5e844521b35d4dc10cd52a0ce5b6f46c104ae30f
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-10-24 19:30:58 +0000

    configured webpack-dev-server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -12,6 +15,32 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/'),
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
             ],
-    }
+    },
+    
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+         disableHostCheck: true
+    },
+    
+    plugins: [
+    new CopyWebpackPlugin({
+          patterns: [
+                    {from: path.resolve(__dirname, 'index.html'),
+                    to: path.resolve(__dirname, 'dist')}
+              ]
+      })
+  ]
 }
+
+  

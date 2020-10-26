4df03f3922a7ea2270c5f0aebe5a8f604c50bcbb
blackpintz <rosewanjohi24@gmail.com>
2020-10-27 00:38:54 +0300

    configured webpack for the index.html

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+};
